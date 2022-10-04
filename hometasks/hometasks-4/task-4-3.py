9627f6759d25c6e59ae1a39946949b9052cba9d2
shakuov_nursultan <priestnurs7@gmail.com>
2022-10-04 20:46:38 +0600

    finished task-4-3

---
+++ hometasks/hometasks-4/task-4-3.py
@@ -0,0 +1,15 @@
+from random import randint
+
+def create_list(size, m, n):
+    return [randint(m, n) for i in range(size)]
+
+def get_unic_value(list):
+    return [i for i in set(list)]
+
+size = 10
+m = 1
+n = 10
+
+origin = create_list(size, m, n)
+print(origin)
+print(get_unic_value(origin))
